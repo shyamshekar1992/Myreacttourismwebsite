2ed3bfd357b96447f248a1cb98fe9fb5d96b351b
shyamshekar1992 <shyamshekar1992@gmail.com>
2021-02-18 21:10:54 +0100

    deploy ready

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
@@ -87,5 +87,9 @@ app.use('/api', require('./routes/auth'));
 // router.post("/", sendEmail);
 
 
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
